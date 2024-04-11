77c3b6700c4a0d382664422c17bf0d02f1b665af
Julian Panici <103546809+julianpanici@users.noreply.github.com>
2024-04-11 10:42:21 -0400

    Fixed titles and added cool hover

--- src/components/BestSeller.vue
+++ src/components/BestSeller.vue
@@ -1,17 +1,17 @@
 <template>
   <v-container>
-            <v-row>
-              <v-col
-                v-for="product in products"
-                :key="product.id"
-                cols="12"
-                sm="6"
-                md="4"
-                lg="3">
-                <StoreItem :product="product"></StoreItem>
-              </v-col>
-            </v-row>
-          </v-container>
+    <v-row>
+      <v-col
+        v-for="product in products"
+        :key="product.id"
+        cols="12"
+        sm="6"
+        md="4"
+        lg="3">
+        <StoreItem :product="product"></StoreItem>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,7 +8,7 @@
         sm="6"
         md="4"
         lg="3">
-        <StoreItem :product="product"></StoreItem>
+        <StoreItem id="storeItem" :product="product"></StoreItem>
       </v-col>
     </v-row>
   </v-container>
